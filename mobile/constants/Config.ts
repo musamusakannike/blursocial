f953201ccbbbe2b6a0d38a4789ef0981bb10b01d
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2026-05-29 06:54:39 +0100

    removed mobile app

--- mobile/constants/Config.ts
+++
@@ -1,7 +0,0 @@
-const FALLBACK_URL = 'https://blursocial.codiac.online';
-
-export const API_URL =
-  process.env.EXPO_PUBLIC_API_URL || process.env.API_URL || FALLBACK_URL;
-
-export const SOCKET_URL =
-  process.env.EXPO_PUBLIC_SOCKET_URL || process.env.SOCKET_URL || FALLBACK_URL;
